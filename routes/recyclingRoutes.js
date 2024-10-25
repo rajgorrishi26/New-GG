ba26a8610d938f3753f504e6b15fd089df792267
Rishi Rajgor <rishirajgor2610@gmail.comm>
2024-10-25 15:32:16 +0530

    Add multer in recycle route

--- routes/recyclingRoutes.js
+++ routes/recyclingRoutes.js
@@ -2,18 +2,27 @@ const express = require('express');
 const router = express.Router();
 const recyclingController = require('./../controllers/recyclingController');
 const authMiddleware = require('../middleware/authMiddleware');
+const upload = require('../middleware/multer'); // Import the multer middleware
 
 // Get all recycled products
 router.route('/recycled-products').get(authMiddleware, recyclingController.GetAllProducts);
 
 // Create a new recycled product
-router.route('/recycled-products').post(authMiddleware, recyclingController.CreateProduct);
+router.route('/recycled-products').post(
+    authMiddleware,
+    upload.array('images', 5), // Use multer to handle multiple image uploads
+    recyclingController.CreateProduct
+  );
 
 // Get a specific recycled product by ID
 router.route('/recycled-products/:id').get(authMiddleware, recyclingController.GetProduct);
 
 // Update a specific recycled product by ID
-router.route('/recycled-products/:id').put(authMiddleware, recyclingController.UpdateProduct);
+router.route('/recycled-products/:id').put(
+    authMiddleware,
+    upload.array('images', 5), // Use multer to handle multiple image uploads
+    recyclingController.UpdateProduct
+  );
 
 // Delete a specific recycled product by ID
 router.route('/recycled-products/:id').delete(authMiddleware, recyclingController.DeleteProduct);
